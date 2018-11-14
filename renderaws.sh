5093c80848f094f5ead2f0fba75ddc3883a69339
Steve Norledge <steve.norledge@alces-software.com>
2018-11-14 11:56:21 +0000

    - wip

--- renderaws.sh
+++ renderaws.sh
@@ -3,11 +3,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 OUTPUTBASE=$DIR/rendered/
 
 #aws
-mkdir -p $OUTPUTBASE/aws/domain/
-underware render $DIR/orchestration/aws/domain.yaml > $OUTPUTBASE/aws/domain/orchestration.yaml
+mkdir -p $OUTPUTBASE/aws/orchestration/
+underware render $DIR/orchestration/aws/main.yaml > $OUTPUTBASE/aws/orchestration/main.yaml
 nodeattr -n all | while read NODE; do
   mkdir -p $OUTPUTBASE/aws/$NODE/
-  underware render $DIR/orchestration/aws/machine.yaml > $OUTPUTBASE/aws/$NODE/orchestration.yaml $NODE
+#  underware render $DIR/orchestration/aws/machine.yaml > $OUTPUTBASE/aws/$NODE/orchestration.yaml $NODE
   cd $DIR
   find deployment/ | while read f; do 
     if [ -f $f ]; then 
---
+++ sync.sh
@@ -0,0 +1,1 @@
+s3cmd sync -P --delete-removed rendered s3://stevenorledge/
